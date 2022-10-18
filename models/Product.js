5615a4db58232dd095d30924a70ba0e5fbf4bcc7
Eric Nguyen <15enguyen4@gmail.com>
2022-10-17 17:00:19 -0700

    user routes and auth, working on product

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema(
     title:{type: String, required: true, unique: true},
     description: {type: String, required: true},
     image: {type: String, required: true},
-    categories: {type: Array},
+    category: {type: String},
     size: {type: String},
     color: {type: String},
     price: {type: Number, required: true},
