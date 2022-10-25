73c73d3fe7fe24cfd064b86614ed42148071a5a8
Eric Nguyen <15enguyen4@gmail.com>
2022-10-25 13:25:17 -0700

    getting err 500, payment token not being sent to server side

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ mongoose
   console.log(err);
 });
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("api/checkout", stripeRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log("Backend server is running!")
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,19 +1,21 @@
-const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_KEY)
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-router.post("/payment", (req,res)=>{
+router.post("/payment", (req, res) => {
   stripe.charges.create(
     {
-    source: req.body.tokenId,
-    amount: req.body.amount,
-    currency: "usd",
-    }, (stripeErr, stripeRes)=>{
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
       if (stripeErr) {
         res.status(500).json(stripeErr);
-      } else{
+      } else {
         res.status(200).json(stripeRes);
       }
-    });
+    }
+  );
 });
 
 
